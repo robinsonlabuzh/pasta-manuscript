3fcf1c47c28eadcfcf238e5b9ccb7bfcb3ca9655
sgunz <samuel.gunz@uzh.ch>
2024-11-28 13:57:10 +0100

    update suppl figure

--- code/manuscript-code-lattice.R
+++ code/manuscript-code-lattice.R
@@ -8,6 +8,7 @@ library(tmap)
 library(patchwork)
 library(dplyr)
 library(tidyr)
+library(scales)
 
 source("code/utils.R")
 
@@ -31,21 +32,10 @@ head(colData(spe))
 hist(colData(spe)$sum, breaks = 25)
 qc_lib_size <- colData(spe)$sum < 600
 
-# check spatial pattern of discarded spots
-plotQC(spe,
-  type = "spots",
-  discard = "qc_lib_size"
-)
-
 # plot library size vs. number of cells per spot
 hist(colData(spe)$detected, breaks = 20)
 qc_detected <- colData(spe)$detected < 2000
 
-# check spatial pattern of discarded spots
-plotQC(spe,
-  type = "spots",
-  discard = "qc_detected"
-)
 
 # histogram of mitochondrial read proportions
 hist(colData(spe)$subsets_mito_percent, breaks = 20)
@@ -54,11 +44,6 @@ hist(colData(spe)$subsets_mito_percent, breaks = 20)
 qc_mito <- colData(spe)$subsets_mito_percent > 28
 table(qc_mito)
 
-# check spatial pattern of discarded spots
-plotQC(spe,
-  type = "spots",
-  discard = "qc_mito"
-)
 
 discard <- qc_lib_size | qc_detected | qc_mito
 
@@ -78,216 +63,24 @@ plotVisium(spe, fill = "Nrgn", assay = "logcounts", image = FALSE)
 colData(spe)$sum <- colSums(counts(spe))
 colData(spe)$logsum <- log(colSums(counts(spe)))
 
-
-plotSpots(spe, annotate = "detected") +
-  scale_colour_gradient2() +
-  geom_point(size = 1)
-
-plotSpots(spe, annotate = "logsum") +
-  scale_colour_viridis() +
-  geom_point(size = 1.5)
-
-plotSpots(spe, annotate = "detected") +
-  scale_color_distiller(palette = "RdYlBu") +
-  geom_point(size = 1.5)
-
-plotSpots(spe, annotate = "logsum") +
-  scale_color_distiller(palette = "RdYlBu") +
-  geom_point(size = 1.5)
-
-pLibSize <- plotSpots(spe, annotate = "sum") +
-  scale_color_distiller(palette = "YlGnBu", direction = 1) +
-  geom_point(size = 1) +
-  labs(color = "Library\nsize", title = "") +
-  theme_void()
-
-pDetected <- plotSpots(spe, annotate = "detected") +
-  scale_color_distiller(palette = "YlGnBu", direction = 1) +
-  geom_point(size = 1) +
-  labs(color = "Genes\ndetected", title = "") +
-  theme_void()
-
-
-plotSpots(spe, annotate = "sum") +
-  scale_color_distiller(palette = "YlGn", direction = 1) +
-  geom_point(size = 1)
-
-
-plotVisium(spe, fill = "sum", trans = "log")
-
-saveRDS(spe, "data/Visium_mouseCorona.rds")
-
-# create sf object
+#saveRDS(spe, "data/Visium_mouseCorona.rds")
+#create sf object
 
 coords <- as.data.frame(spatialCoords(spe))
-
 coords$pxl_row_in_fullres <- -coords$pxl_row_in_fullres
-
 spsf <- st_as_sf(coords,
-  coords = c("pxl_col_in_fullres", "pxl_row_in_fullres")
+                 coords = c("pxl_col_in_fullres", "pxl_row_in_fullres")
 )
 
+# Select gene "Nrgn"
 Nrgn <- logcounts(spe)["Nrgn", ] |>
   as.matrix() |>
   as.data.frame()
 
 colnames(Nrgn) <- "Nrgn"
 
+# create object
 spsf <- cbind(spsf, colData(spe), Nrgn)
-
-
-plot(spsf)
-
-# find 5 nearest neighbours
-st_buffer(st_geometry(spsf), dist = 80) |>
-  poly2nb(snap = 10) |>
-  nb2listw() -> direct_neigbours
-
-plot(st_buffer(st_geometry(spsf), dist = 70), border = "grey")
-plot(direct_neigbours, st_geometry(spsf), add = TRUE)
-
-
-
-glance_htest <- function(ht) {
-  c(ht$estimate,
-    "Std deviate" = unname(ht$statistic),
-    "p.value" = unname(ht$p.value)
-  )
-}
-# global moran's I
-moran.test(spsf[["detected"]], listw = direct_neigbours, randomisation = FALSE) %>% glance_htest()
-
-# local moran's I
-loc <- localmoran(spsf[["detected"]], listw = direct_neigbours)
-# extract the effect size
-locEffect <- loc[, 1]
-# extract the p-value and adjust for multiple testing
-p.val.adj <- loc[, 5] |> p.adjust("BH")
-
-spsf$locEffect <- locEffect
-spsf$p.val.adj <- p.val.adj
-
-# plot(spsf)
-
-
-pLocEff_direct <- ggplot() +
-  geom_sf(data = spsf, aes(color = locEffect), size = 1) +
-  scale_color_distiller(palette = "RdYlBu", limits = c(-2, 8)) +
-  theme_void()
-
-pLocEff_direct
-
-
-# ggsave("misc/visium_overview_poster.pdf", width = 8, height = 6)
-
-
-plot(st_buffer(st_geometry(spsf), dist = 70), border = "grey")
-plot(direct_neigbours, st_geometry(spsf), add = TRUE)
-
-
-#
-spsf %>%
-  knearneigh(k = 2) %>%
-  knn2nb() %>%
-  nb2listw(style = "W") -> knn5
-
-glance_htest <- function(ht) {
-  c(ht$estimate,
-    "Std deviate" = unname(ht$statistic),
-    "p.value" = unname(ht$p.value)
-  )
-}
-# global moran's I
-moran.test(spsf[["detected"]], listw = knn5, randomisation = FALSE) %>% glance_htest()
-
-# local moran's I
-loc <- localmoran(spsf[["detected"]], listw = knn5)
-# extract the effect size
-locEffect <- loc[, 1]
-# extract the p-value and adjust for multiple testing
-p.val.adj <- loc[, 5] |> p.adjust("BH")
-
-spsf$locEffect <- locEffect
-spsf$p.val.adj <- p.val.adj
-
-fill_range <- seq(-3, 8, by = 0.1)
-
-pLocEff_knn <- ggplot() +
-  geom_sf(data = spsf, aes(color = locEffect), size = 1) +
-  scale_color_distiller(palette = "RdYlBu", limits = c(-2, 8)) +
-  theme_void()
-
-pLocEff_knn
-
-####
-# Find out neighbour size
-####
-
-st_coordinates(spsf) |> head()
-
-# sort by X
-st_coordinates(spsf) |>
-  as.data.frame() |>
-  arrange(X, Y) |>
-  filter(X == 3137) |>
-  mutate(Y.difference = Y - lag(Y))
-
-st_coordinates(spsf) |>
-  as.data.frame() |>
-  arrange(Y, X) |>
-  filter(Y == -8075) |>
-  mutate(X.difference = X - lag(X))
-
-st_buffer(st_geometry(spsf), dist = 70) |> plot()
-
-st_buffer(st_geometry(spsf), dist = 70) |> poly2nb()
-
-
-## gene
-p <- pDetected + pLocEff_direct + pLocEff_knn
-p
-
-
-# Nrgn
-
-pDetected <- plotVisium(spe,
-  fill = "Nrgn", assay = "logcounts",
-  image = FALSE, facets = NULL
-) +
-  scale_fill_distiller(palette = "BuPu", direction = 1) +
-  geom_point(size = 5) +
-  labs(color = "Nrgn", title = "") +
-  theme_void() +
-  labs(title = "", subtitle = "")
-
-pDetected
-
-
-spsf %>%
-  knearneigh(k = 10) %>%
-  knn2nb() %>%
-  nb2listw(style = "W") -> knn5
-
-# local moran's I
-loc <- localmoran(spsf[["Nrgn"]], listw = knn5)
-# extract the effect size
-locEffect <- loc[, 1]
-# extract the p-value and adjust for multiple testing
-p.val.adj <- loc[, 5] |> p.adjust("BH")
-
-spsf$locEffect <- locEffect
-spsf$p.val.adj <- p.val.adj
-
-fill_range <- seq(-3, 8, by = 0.1)
-
-pLocEff_knn <- ggplot() +
-  geom_sf(data = spsf, aes(color = locEffect), size = 1) +
-  scale_color_distiller(palette = "RdYlBu") +
-  theme_void()
-
-pLocEff_knn
-
-
 
 st_buffer(st_geometry(spsf), dist = 80) |>
   poly2nb(snap = 10) |>
@@ -309,21 +102,28 @@ spsf$p.val.adj <- p.val.adj
 
 fill_range <- seq(-3, 8, by = 0.1)
 
-pLocEff_direct <- ggplot() +
-  geom_sf(data = spsf, aes(color = locEffect), size = 0.3) +
-  scale_color_distiller(
-    palette = "RdYlBu", direction = -1,
-    limits = c(-4, 4)
-  ) +
-  labs(color = "locI(Nrgn)") +
+pDetected <- ggplot() +
+  geom_sf(data = spsf, aes(color = Nrgn), size = 0.7) +
+  scale_color_distiller(palette = "BuPu", direction = 1) +
+  geom_point(size = 0.7) +
+  labs(color = "Nrgn", title = "Expression of Nrgn\n") +
   theme_void()
+
+pLocEff_direct <- ggplot() +
+  geom_sf(data = spsf, aes(color = locEffect), size = 0.7) +
+  #geom_sf(data = spsf, aes(color = locEffect), size = 0.3) +
+  labs(color = "locI(Nrgn)", title = "Local Moran's I\n(Direct neighbours)") +
+  theme_void() +
+  scale_color_gradientn(colours = c("blue","white","red"), 
+                        values = rescale(c(-1.1,0,4.6)),
+                        guide = "colorbar", limits=c(-1.1,4.6))
 
 pLocEff_direct
 
 pLocEff_pVal <- ggplot() +
-  geom_sf(data = spsf, aes(color = -log10(p.val.adj)), size = 0.3) +
+  geom_sf(data = spsf, aes(color = -log10(p.val.adj)), size = 0.7) +
   scale_color_distiller(palette = "BuGn", direction = 1) +
-  labs(color = "-log10(adj. p-val.)") +
+  labs(color = "-log10(adj. p-val.)", title = "Local Moran's I\n(Adjusted significance levels)") +
   theme_void()
 
 pLocEff_pVal
@@ -339,13 +139,14 @@ spsf |>
   theme_light()
 
 
+
 pGeneLoc <- spsf |>
   ggplot(aes(x = Nrgn, y = locEffect, fill = -log10(p.val.adj))) +
   geom_point() +
   geom_point(colour = "black", pch = 21) +
   scale_fill_distiller(palette = "BuGn", direction = 1) +
   theme_light() +
-  geom_density_2d(color = "cornflowerblue")
+  geom_density_2d(color = "cornflowerblue", bins = 7)
 
 
 pLocPval <- spsf |>
@@ -354,22 +155,16 @@ pLocPval <- spsf |>
   geom_point(colour = "black", pch = 21) +
   scale_fill_distiller(palette = "BuPu", direction = 1) +
   theme_light() +
-  geom_density_2d(color = "cornflowerblue")
+  geom_density_2d(color = "cornflowerblue", bins = 7)
 
 psupploc <- pGeneLoc + pLocPval
 
-## gene
-p <- pDetected + pLocEff_direct + pLocEff_knn
-p
 
+## Plot
 p2 <- pDetected + pLocEff_direct + pLocEff_pVal
 
-p2
-
-
 ## Example neighbourhood
-##
-##
+## Source: https://pachterlab.github.io/voyager/articles/vig4_cosmx.html
 (sfe <- HeNSCLCData())
 
 # Empty cells
@@ -389,77 +184,107 @@ rowData(sfe)$is_neg <- neg_inds
 sfe <- logNormCounts(sfe)
 
 sfePoly <- sfe
+sfeDist <- sfe
 
 colGraph(sfe, "knn10") <- findSpatialNeighbors(sfe,
-  method = "knearneigh",
-  dist_type = "idw", k = 10,
-  style = "W"
+                                               method = "knearneigh",
+                                               dist_type = "idw", k = 10,
+                                               style = "W"
+)
+
+colGraph(sfeDist, "distance") <- findSpatialNeighbors(sfeDist,
+                                                      method = "dnearneigh",
+                                                      d1 = 0, d2 = 1000,
+                                                      dist_type = "idw",
+                                                      style = "W"
 )
 
 
 colGraph(sfePoly, "poly2nb") <- colGeometries(sfePoly)[["cellSeg"]] |>
-  poly2nb(snap = 2) |> # snap = 10
+  poly2nb(snap = 5) |> # snap = 10
   nb2listw(zero.policy = TRUE)
 
 
-bbox_use <- st_bbox(c(xmin = 8000, xmax = 15000, ymin = 161000, ymax = 166000))
-
 sfe <- runUnivariate(sfe,
-  features = c("KRT17"),
-  type = "localmoran"
+                     features = c("KRT17"),
+                     type = "localmoran",
+                     zero.policy = TRUE
 )
 
-pKnn <- plotLocalResult(sfe, "localmoran",
-  features = c("KRT17"), ncol = 1,
-  colGeometryName = "cellSeg",
-  bbox = bbox_use
-)
-pKnn <- pKnn + scale_fill_distiller(palette = "RdYlBu", limits = c(-13, 13)) +
-  labs(title = "Local Moran's I\n(Contiguos Neighbours)", fill = "locI(KRT17)")
+bbox_use <- st_bbox(c(xmin = 14000, xmax = 18000, ymin = 158000, ymax = 162000))
+
+# kNN based
+(pKnn <- plotLocalResult(sfe, "localmoran",
+                         features = c("KRT17"), ncol = 1,
+                         colGeometryName = "cellSeg", linewidth = 0.05,
+                         bbox = bbox_use
+))
+
+pKnn <- pKnn + scale_fill_gradientn(colours = c("#0028A5","#FAFAFA","#FFC845", "#BF0D3E"), 
+                                    values = rescale(c(-3,0,5,17)),
+                                    guide = "colorbar", limits=c(-3,17)) +
+  labs(title = "Local Moran's I\n(10 Nearest Neighbours)", fill = "locI(KRT17)")
 
 pKnn
 
 # poly
 sfePoly <- runUnivariate(sfePoly,
-  features = c("KRT17"),
-  type = "localmoran",
-  zero.policy = TRUE
+                         features = c("KRT17"),
+                         type = "localmoran",
+                         zero.policy = TRUE
 )
 
 
-pPoly <- plotLocalResult(sfePoly, "localmoran",
-  features = c("KRT17"), ncol = 1,
-  colGeometryName = "cellSeg",
-  bbox = bbox_use
+(pPoly <- plotLocalResult(sfePoly, "localmoran",
+                          features = c("KRT17"), ncol = 1,
+                          colGeometryName = "cellSeg", linewidth = 0.05,
+                          bbox = bbox_use
+))
+
+
+pPoly <- pPoly + scale_fill_gradientn(colours = c("#0028A5","#FAFAFA","#FFC845", "#BF0D3E"), 
+                                      values = rescale(c(-3,0,5,17)),
+                                      guide = "colorbar", limits=c(-3,17)) +
+  labs(title = "Local Moran's I\n(Contiguos Neighbours)", fill = "locI(KRT17)") 
+
+
+# distance
+sfeDist <- runUnivariate(sfeDist,
+                         features = c("KRT17"),
+                         type = "localmoran",
+                         zero.policy = TRUE
 )
 
 
-pPoly <- pPoly + scale_fill_distiller(palette = "RdYlBu", limits = c(-13, 13)) +
-  labs(title = "Local Moran's I\n(Contiguos Neighbours)", fill = "locI(KRT17)")
-
-pPoly
-
-
-pLocalDiff <- (pKnn + pPoly) +
-  plot_layout(guides = "collect")
-
-pLocalDiff
+(pDist <- plotLocalResult(sfeDist, "localmoran",
+                          features = c("KRT17"), ncol = 1,
+                          colGeometryName = "cellSeg", linewidth = 0.05,
+                          bbox = bbox_use
+))
 
 
-fig2 <- (pLocalDiff / p2) +
+### Figure 2 in main manuscript
+
+pDist <- pDist + scale_fill_gradientn(colours = c("#0028A5","#FAFAFA","#FFC845", "#BF0D3E"), 
+                                      values = rescale(c(-3,0,5,17)),
+                                      guide = "colorbar", limits=c(-3,17)) +
+  labs(title = "Local Moran's I\n(Neighbours in 1000 pixel distance)", fill = "locI(KRT17)") +
+  theme_classic()
+
+fig2 <- pDetected + pLocEff_direct + pLocEff_pVal + pPoly + pKnn + pDist +
+  plot_layout(nrow = 2, ncol = 3) +
   plot_annotation(tag_levels = "A") &
   theme(
     plot.tag.position = c(0, 1),
-    plot.tag = element_text(size = 10, hjust = 0, vjust = 0)
+    plot.tag = element_text(size = 12, hjust = 0, vjust = 0)
   )
 
 fig2
 
-ggsave("outs/localMoranOverview.pdf", fig2, width = 8, height = 6)
+ggsave("outs/localMoranOverview.pdf", fig2, width = 12, height = 8)
 
 
-###
-
+### Supplementary Figure Lattice part
 data.frame(
   knn = localResult(sfe, "localmoran", "KRT17")[, "Ii"],
   cont = localResult(sfePoly, "localmoran", "KRT17")[, "Ii"]
@@ -470,29 +295,62 @@ data.frame(
   facet_grid(neighbourhood ~ .)
 
 
+logExpHist <- data.frame(KRT17 = logcounts(sfe)["KRT17",]) |> 
+  ggplot(aes(x = KRT17)) +
+  geom_histogram() +
+  theme_light()
+
+# Inspection of distribution of resulting values
+localResult(sfe, "localmoran", "KRT17")[, "Ii"] |> 
+  round(digits = 5) |> 
+  table() |> sort(decreasing = TRUE) |> 
+  head(n = 10)
+
+localResult(sfePoly, "localmoran", "KRT17")[, "Ii"] |> 
+  round(digits = 5) |> 
+  table() |> sort(decreasing = TRUE) |> 
+  head(n = 10)
 
 pknncont <- data.frame(
   knn = localResult(sfe, "localmoran", "KRT17")[, "Ii"],
   cont = localResult(sfePoly, "localmoran", "KRT17")[, "Ii"]
 ) |>
-  ggplot(aes(x = knn, y = cont)) +
-  geom_point(size = 0.5) +
-  geom_abline(intercept = 0, slope = 1, col = "cornflowerblue") +
+  mutate(zero_neigh = ifelse((cont == 0 & knn != 0), "red", "black")) |> 
+  ggplot(aes(x = knn, y = cont, color = zero_neigh)) +
+  geom_point(size = 0.5, show.legend = FALSE) +
+  geom_abline(intercept = 0, slope = 1, col = "seagreen") +
   theme_light() +
   labs(
     x = "Local Moran's I \n(10 Nearest Neighbours)",
     y = "Local Moran's I \n(Contiguos Neighbours)"
   ) +
   coord_fixed(ratio = 1) +
-  geom_density_2d(color = "cornflowerblue")
-
-
+  geom_density_2d(color = "cornflowerblue") +
+  scale_color_manual(values = c("black", "red"))
+  
+pDistcont <- data.frame(
+  dist = localResult(sfeDist, "localmoran", "KRT17")[, "Ii"],
+  cont = localResult(sfePoly, "localmoran", "KRT17")[, "Ii"]
+) |>
+  mutate(zero_neigh = ifelse((cont == 0 & dist != 0), "red", "black")) |> 
+  ggplot(aes(x = dist, y = cont, color = zero_neigh)) +
+  geom_point(size = 0.5, show.legend = FALSE) +
+  geom_abline(intercept = 0, slope = 1, col = "seagreen") +
+  geom_density_2d(color = "cornflowerblue") +
+  theme_light() +
+  labs(
+    x = "Local Moran's I \n(Neighbours in 1000 pixel distance)",
+    y = "Local Moran's I \n(Contiguos Neighbours)"
+  ) +
+  coord_fixed(ratio = 1) +
+  scale_color_manual(values = c("black", "red"))
+  
 data.frame(
   knn = localResult(sfe, "localmoran", "KRT17")[, "Ii"],
   cont = localResult(sfePoly, "localmoran", "KRT17")[, "Ii"]
 ) |>
   ggplot(aes(x = knn, y = cont)) +
-  geom_point(size = 0.5) +
+  geom_point(size = 0.5, show.legend = FALSE) +
   geom_abline(intercept = 0, slope = 1, col = "cornflowerblue") +
   theme_light() +
   labs(
@@ -503,23 +361,24 @@ data.frame(
   geom_smooth(method = "lm", col = "red")
 
 
-<<<<<<< Updated upstream
-layout <- "
+# layout for supplementary figure
+layout <- 
+"
 AAAA
 AAAA
 AAAA
-BBCC
-BBCC
-BBCC
+AAAA
+AAAA
+BBBB
 "
 
-psupp <- pknncont + pGeneLoc + pLocPval +
+# supplementary figure
+psupp <- (pGeneLoc + pLocPval + pknncont + pDistcont) /  logExpHist +
   plot_layout(design = layout) +
-=======
-psupp <- pGeneLoc + pLocPval + pknncont + pDistcont +
->>>>>>> Stashed changes
   plot_annotation(tag_levels = "A")
 
+#plot
 psupp
 
-ggsave("outs/localMoranOverviewSupp.pdf", psupp, width = 9, height = 6)
+ggsave("outs/localMoranOverviewSupp.pdf", psupp, width = 8, height = 8)
+
